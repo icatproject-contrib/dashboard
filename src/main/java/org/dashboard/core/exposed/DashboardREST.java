c68b820664a01314190024130596d6f5ee40b3c0
Alex Potter-Dixon <apd123@live.co.uk>
2015-11-06 19:23:29 +0000

    Restful interface initial implimentation.

--- src/main/java/org/dashboard/core/exposed/DashboardREST.java
+++ src/main/java/org/dashboard/core/exposed/DashboardREST.java
@@ -5,15 +5,22 @@
  */
 package org.dashboard.core.exposed;
 
+import java.io.ByteArrayInputStream;
+import java.net.URISyntaxException;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
 import javax.annotation.Resource;
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.ejb.TransactionManagement;
 import javax.ejb.TransactionManagementType;
+import javax.json.Json;
+import javax.json.stream.JsonParser;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -26,6 +33,9 @@ import org.dashboard.core.manager.DashboardException;
 import org.dashboard.core.manager.DashboardException.DashboardExceptionType;
 import org.dashboard.core.manager.EntityBeanManager;
 import org.dashboard.core.manager.PropsManager;
+import org.icatproject.icat.client.ICAT;
+import org.icatproject.icat.client.IcatException;
+import org.icatproject.icat.client.Session;
 
 @Path("/")
 @Stateless
@@ -72,16 +82,50 @@ public class DashboardREST {
    
         @POST
         @Path("session/login")
-        public String login(@FormParam("json") String loginString) throws DashboardException{
+        public String login(@FormParam("json") String loginString) throws DashboardException, URISyntaxException, IcatException{
             if (loginString == null) {
 			throw new DashboardException(DashboardExceptionType.BAD_PARAMETER, "json must not be null");
 		}
             
+            ICAT icat = new ICAT(properties.getICATUrl());
+            Map<String, String> credentials = new HashMap<>();
+            String plugin = null;
+            String userName = null;
+            try (JsonParser parser = Json.createParser(new ByteArrayInputStream(loginString.getBytes()))) {
+                String key = null;
+			boolean inCredentials = false;
+
+			while (parser.hasNext()) {
+				JsonParser.Event event = parser.next();
+				if (event == JsonParser.Event.KEY_NAME) {
+					key = parser.getString();
+				} else if (event == JsonParser.Event.VALUE_STRING) {
+					if (inCredentials) {
+						credentials.put(key, parser.getString());
+					} else {
+						if (key.equals("plugin")) {
+							plugin = parser.getString();
+						}
+					}
+				} else if (event == JsonParser.Event.START_ARRAY && key.equals("credentials")) {
+					inCredentials = true;
+				} else if (event == JsonParser.Event.END_ARRAY) {
+					inCredentials = false;
+				}
+			}
+            }            
+            
+            Session session = icat.login(plugin, credentials);
+            
+            userName = session.getUserName();
+            
+            
+        return null;
 
         }
 
 
-        @POST
+        @DELETE
         @Path("session/logout")
         public String logut(){
             return null;
