93b0910565d0ef071d4b161737ac31d6a71e1907
yqa41233 <yqa41233@SCLT053.escdc.fed.cclrc.ac.uk>
2016-01-23 10:14:39 +0000

    Modification to attempt to fix the CORS response filter issues.

--- src/main/java/org/icatproject/dashboard/exposed/CORSResponseFilter.java
+++ src/main/java/org/icatproject/dashboard/exposed/CORSResponseFilter.java
@@ -5,19 +5,19 @@ import java.io.IOException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.Provider;
 
 @Provider
-public class CORSResponseFilter implements ContainerResponseFilter {
-
-	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
-			throws IOException {
-
-		MultivaluedMap<String, Object> headers = responseContext.getHeaders();
-		headers.add("Access-Control-Allow-Origin", "*");
-		headers.add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
-                headers.add("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-                
-	}
+@PreMatching
+public class CORSResponseFilter implements ContainerResponseFilter { 
+    @Override
+    public void filter( ContainerRequestContext requestCtx, ContainerResponseContext responseCtx ) throws IOException {
+      
+ 
+        responseCtx.getHeaders().add( "Access-Control-Allow-Origin", "*" );        
+        responseCtx.getHeaders().add( "Access-Control-Allow-Methods", "GET, POST, DELETE, PUT" );
+        responseCtx.getHeaders().add( "Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    }
 }
