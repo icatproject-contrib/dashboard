be529a1d156dac5ff35823dd994cea90535de5f4
Alex Potter-Dixon <Alex Potter-Dixon@APD-PC>
2016-02-20 21:18:46 +0000

    Closes #28, #27, #17, #10, #16

--- yo/app/scripts/controllers/downloadCtrl.js
+++ yo/app/scripts/controllers/downloadCtrl.js
@@ -44,8 +44,15 @@
         	});
 
 		self.updatePage = function(){	
-		
-			var startDate = Date.parse(self.startDate);
+
+			//Have to set the time to midnight otherwise will use current time.
+     		var startDate = moment(self.startDate);			
+
+			startDate.set('hour','00');
+			startDate.set('minute','00');
+			startDate.set('second','00');
+
+						
 			var endDate = Date.parse(self.endDate);
 			var userName = self.userName;
 			var method = self.selectedMethod;	
@@ -60,8 +67,7 @@
 			var localDownloadLocation = DownloadService.getLocalDownloadLocation(startDate,endDate, userName, method);
 			var downloadEntityAge = DownloadService.getDownloadEntityAge(startDate,endDate,userName,method);
 
-			downloadEntityAge.then(function(responseData){
-						
+			downloadEntityAge.then(function(responseData){					
 
 				
 				var age  = _.map(responseData, function(responseData){
@@ -72,16 +78,11 @@
 					return responseData.number;
 				});
 
-				age.unshift("x");		
+				age.unshift("Days old");		
 							
-				number.unshift('number of Files');
-				
-				
-
-				
+				number.unshift('Number of Files');				
 				
 				self.downloadEntityAge = [age,number];
-					
 				
 				
 
@@ -195,7 +196,7 @@
 
 	    		dates.unshift("x");		
 							
-				numbers.unshift('Count');	    		
+				numbers.unshift('Number of Downloads');	    		
 					
 			    self.downloadCount = [dates,numbers];
 
@@ -240,15 +241,11 @@
 		    			
 					});
 		    	
-		    	self.downloadBandwidth = bandwidthData;
-
-		    	//Bandwidth metrics
-
-		    	var bandwidthMetrics = getBandwidthMetrics(responseData);
+		    	self.downloadBandwidth = bandwidthData;  	
 		    	
-
-		    	self.lowestBandwidth = bandwidthMetrics[1]+"MB/S";
-		    	self.heighestBandwidth = bandwidthMetrics[0]+"MB/S";
+		    	
+		    	self.highestBandwidth = Math.max.apply(Math,responseData.map(function(data){return data.bandwidth;}))+"MB/S";
+		    	self.lowestBandwidth  = Math.min.apply(Math,responseData.map(function(data){return data.bandwidth;}))+"MB/S";
 
 		    	
 
@@ -256,31 +253,42 @@
 
 			
 	    	downloadSize.then(function(responseData){
+
 	    		var data = responseData;
 
 	    		var dates  = _.map(data, function(data){
 					return data.date;
 				});
 
-				var numbers = _.map(data, function(data){
-					return Math.round(data.number/1000000);
+				var byteArray = _.map(data, function(data){
+					
+					return Math.round(data.number);
 				});
+
+				//Need to format the bytes into human readable format
+				var formattedData = byteArrayToSize(byteArray)
+
+				
 
 				//Download number metrics
 
 		    	self.getTotalnumber = function(){
-		    		var total = 0;    		    			    		
+		    		var total = 0.0;    		    			    		
 		    		
-		    		for(var i=1;i<numbers.length;i++){	    			
-		    			total +=numbers[i];
+
+		    		for(var i=0;i<byteArray.length;i++){
+		    			if(total!==null){	    			
+		    				total +=byteArray[i];
+		    			}
 		    		}
 
-		    		return total +" Megabytes";
+		    		return bytesToSize(total);
 		    	}
 	    	
-
+		    	//Busiest day metric
 		    	self.getBusiestDaynumber = function(){
-		    		var data = [dates,numbers];
+
+		    		var data = [dates,byteArray];
 
 		    		var busiestIndex = 0;
 		    		var largestDay = 0;	    		
@@ -292,15 +300,22 @@
 		    			}
 		    		}
 
-		    		return data[0][busiestIndex] +" with "+largestDay +" Megabytes";
+		    		return data[0][busiestIndex] +" with "+bytesToSize(largestDay);
 
 		    	}	    	
 
+		    	//Adding on to the beginning of the array to allow c3 to read the data
 				dates.unshift("x");		
 							
-				numbers.unshift('Count');				
+				var dataForGraph = formattedData[0];
+				dataForGraph.unshift(formattedData[1]);
+		
 
-	    		self.downloadSize = [dates,numbers];
+	    		self.downloadSize = { 	
+	    			data:[dates,dataForGraph],
+	    			byteFormat:formattedData[1]
+
+	    		};
 	    	});
 
 			
@@ -315,33 +330,61 @@
  
 	}
 
-	/*Calculates the highest and lowest bandiwdth from the provided data.
-	* 
-	*/
-	function getBandwidthMetrics(data) {     		
 
-    		var highest = 0;
-    		var lowest = 9999999;
-    		
-	    	data.forEach( function(download){
-	    		if(download.bandwidth >highest){
-	    			highest = download.bandwidth;
-	    		}
-	    		if(download.bandwidth < lowest && download.bandwidth !=='0.0'){
-	    			lowest = download.bandwidth;
-	    		}
-	    	});
+	function bytesToSize(bytes) {
+   		var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+   		if (bytes == 0) return '0 Byte';
 
-	    	//Don't want 9999999 returned as not correct.
-	    	if(lowest === 9999999){
-	    		lowest=0;
-	    	}
+   		var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
 
-	    	var result = [lowest,highest];
+   		return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+};
 
-	    	return result;
-	}
 
+	/*
+	 * Function to convert an array of bytes into an array human readable form bytes. 
+	 *
+	 */
+	 
+	function byteArrayToSize(byteArray) {
+   		var sizes = ['Bytes', 'Kilobytes', 'Megabytes', 'Gigabytes', 'Terabytes'];
+
+   		
+
+   		var max = 0;
+   		var formattedArray = [];
+   		
+   		//Find the highest bytes value to use as a marker to convert the rest of the bytes into. Quicker to use a for loop then Math.max.
+   		for(var i=0;i<byteArray.length;i++){
+   			var bytes = byteArray[i];
+   			if(bytes>max){
+   				max = bytes;
+   			}
+   		}
+   		
+   		//This then selects what type of byte it should be e.g. byte or KB. byteFormat refers to the position in the sizes array.
+   		var byteFormat = parseInt(Math.floor(Math.log(max) / Math.log(1024)));
+
+
+   		//With the known type of byte we now need to format each one and add it to the array.
+   		for(var i=0;i<byteArray.length;i++){
+   			var bytes = byteArray[i];
+   			   			
+   			if(parseInt(bytes) === 0){
+   				//Null is placed so that c3 ignores those values
+   				formattedArray.push("null");
+   			}
+   			else{
+   				//Convert it to the correct byte value and then round to 2 decimal places.
+   				formattedArray.push((Math.round(bytes / Math.pow(1024, byteFormat)*100)/100));
+   			}
+   		}
+
+   		   		   
+   		return [formattedArray,sizes[byteFormat]];
+};
+
+	
 	function getDateArray(startDate,stopDate) {
 		var dateArray = [];
 	    var currentDate = moment(startDate);
