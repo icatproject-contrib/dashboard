eed5cc639234e79566e662bd20c740e890d3197a
Alex Potter-Dixon <Alex Potter-Dixon@APD-PC>
2016-02-20 21:19:08 +0000

    Removed redundant code.

--- yo/app/scripts/controllers/indexCtrl.js
+++ yo/app/scripts/controllers/indexCtrl.js
@@ -3,9 +3,9 @@
 	angular.module('dashboardApp')
 		   .controller('IndexController', IndexController);
 
-	IndexController.$inject = ['$sessionStorage','$scope'];
+	IndexController.$inject = ['$sessionStorage','$rootScope'];
 
-	function IndexController($sessionStorage,$scope){
+	function IndexController($sessionStorage,$rootScope){
 
 		
 		
@@ -15,35 +15,9 @@
 			return !(_.isEmpty($sessionStorage.sessionData));
 		}
 
-		//Date range manipulation so data is defaulted to 10 days ago.
+		$rootScope.baseURL = 'api/v1/';
 
-		var currentDate = moment();
-		var initialDate = moment().subtract(10,'days');
-
-		$scope.myDateRange = { startDate: initialDate, endDate: currentDate };
-
-
-		/* Workaround to deal with datepicker use case: 
-			When a user selects the start date as the current date
-			then the current time will be selected which isn't good. It needs to be
-			00:00:00. May be removed if user asks for time.
-		*/
-		$scope.$watch('myDateRange',function(newDate){
-			var start = moment(newDate.startDate);
-			
-			
-			if(start.get('date')===moment().get('date')) {
-				var newStartDate = moment();
-				newStartDate.set('hour',00);
-				newStartDate.set('minute',00);
-				newStartDate.set('second',00);
-				$scope.myDateRange.startDate = newStartDate;
 		
-			}
-			
-
-
-		});
 	}		   
 
 
