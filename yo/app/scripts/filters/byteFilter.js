bc8009b62de952d08570fe3e04e6adfa361cf787
Gowland <tom.gowland@stfc.ac.uk>
2017-01-16 11:33:05 +0000

    Accounts for scientific notation to fix #87

--- yo/app/scripts/filters/byteFilter.js
+++ yo/app/scripts/filters/byteFilter.js
@@ -29,8 +29,8 @@
                     }
                     else{
                         var log = getLog(value);
-
-                        var formattedValue = convertBytes(value,log) 
+                        
+                        var formattedValue = convertBytes(value,log);
 
                         result = formattedValue+' '+sizes[log];                      
                     }               
@@ -40,9 +40,10 @@
                     var maxValue = args[1];
 
                     result = byteArrayToSizeWithValue(value, maxValue);
-                }               
+                }
+                
 
-		   		return result;                
+                return result;                
             };
 
             //Gets the human readable format array value for sizes.
@@ -54,17 +55,51 @@
             //Converts the bytes into human readable format. bytes is the value to change and 
             // log is the power to convert by.
             function convertBytes(bytes, log){
-
+                    
                 var convertedValue = (bytes / Math.pow(1024, log));
+                
+                var byteValue = convertedValue;
 
-                convertedValue = convertedValue.toString();               
+                convertedValue = convertedValue.toString();
+                
+                                
+                // This is a very small number which has been expressed in scientific notation
+                // (i.e. it uses e^-x) since we cut off the latter half of the string to get
+                // rid of unneeeded decimal numbers, we would ignore the e^-x part unless we deal with it here.
+                if(convertedValue.includes("e")) {
+                    byteValue = toFixed(byteValue);
+                    convertedValue = byteValue.toString();
+                }
+                
                 //Only want to substring values that have decimal places.
                 if(convertedValue.indexOf(".") > -1){
                     convertedValue = convertedValue.slice(0, (convertedValue.indexOf("."))+3);
-                }                
+                }
+
                 
                 return parseFloat(convertedValue);
-
+            }
+            
+            // This function will take a number in scientific notation and convert it to standard 
+            // notation. 
+            function toFixed(x) {
+                if (Math.abs(x) < 1.0) {
+                    // Used for negative powers
+                    var e = parseInt(x.toString().split('e-')[1]);
+                    if (e) {
+                        x *= Math.pow(10,e-1);
+                        x = '0.' + (new Array(e)).join('0') + x.toString().substring(2);
+                    }
+                } else {
+                    // Used for positive powers
+                    var e = parseInt(x.toString().split('+')[1]);
+                    if (e > 20) {
+                        e -= 20;
+                        x /= Math.pow(10,e);
+                        x += (new Array(e+1)).join('0');
+                    }
+                }
+                return x;
             }
 
             //Converts an array of bytes into the correct size with the provided max value.            
@@ -74,7 +109,7 @@
 
                 //This then selects what type of byte it should be e.g. byte or KB. byteFormat refers to the position in the sizes array.
                 var byteFormat = getLog(maxValue);
-
+                
                 //With the known type of byte we now need to format each one and add it to the array.
                 for(var i=0;i<byteArray.length;i++){
                     var bytes = byteArray[i];
